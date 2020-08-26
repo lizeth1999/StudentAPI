28470b3a68b0e9815cd2da59541c9f5005a8f5f4
lizeth jimenez <a2019117291@estudiantes.upsa.edu.bo>
2020-08-26 15:36:25 -0700

    Se creo el API web de estudiante.

---
+++ APIstudent1/APIstudent1/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent1.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Range(1,9999)]
+        public int StudentID { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage ="YOU MUST ENTER THE FIELD {0}")]
+        public string LastName { get; set; }
+
+        [StringLength(30,ErrorMessage ="THE FIELD {0} MUST CONTAIN BETWEEN  {2} AND {1} CHARACTERS",MinimumLength =2)]
+        [Required(ErrorMessage = "YOU MUST ENTER THE FIELD {0}")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}",ApplyFormatInEditMode =true)]
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
